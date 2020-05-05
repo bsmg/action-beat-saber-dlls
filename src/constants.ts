12761495497b86388a3be4c33d1af6a178450d80
Jack Baron <jackmbaron@gmail.com>
2020-05-05 05:20:01 +0100

    Update repo slug

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const REPO_SLUG = 'beat-saber-modding-group/beat-saber-dlls'
+export const REPO_SLUG = 'bsmg/beat-saber-dlls'
 export const REPO_BRANCH = 'master'
 export const VERSION_JSON = 'versions.json'
